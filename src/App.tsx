3ad34e7a97cd11c51e7deb32ea751ffdaa391a62
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-07-25 14:45:38 -0300

    adicionado header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import styled from 'styled-components';
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>hello friend</h1>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
