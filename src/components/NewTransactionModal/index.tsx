334a227b84c41a99bdbb8382c72a3fc281f97119
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-07-26 14:50:04 -0300

    ajustado estilização do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,9 +11,29 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>cadastrar</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
 
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Título"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
