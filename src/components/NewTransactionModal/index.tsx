50d2a550836491cf9c6e48b29bf31e9f37c4a927
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-07-27 18:36:50 -0300

    ajustado cor dos botões entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -43,6 +43,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"            
             onClick={() => { setType('deposit'); }}
             isActive={type === 'deposit'}
+            activeColor="green"
           >            
             <img src={incomeIMG} alt="Entrada" />
             <span>Entrada</span>
@@ -52,6 +53,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"
             onClick={() =>{ setType('withdraw'); }}
             isActive={type === 'withdraw'}
+            activeColor="red"
           >            
             <img src={outcomeIMG} alt="Saída" />
             <span>Saída</span>
