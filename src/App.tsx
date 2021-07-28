3239e468cecd260e4cefc52d67e57809d6b505f8
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-07-28 18:41:32 -0300

    adicionado contexto nos dados das transações

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);  
 
   function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -26,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import incomeIMG from '../../assets/income.svg'
 import outcomeIMG from '../../assets/outcome.svg'
 import totalIMG from '../../assets/total.svg'
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
   return(
     <Container>
       <div>
