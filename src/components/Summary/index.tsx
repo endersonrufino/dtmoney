628075742d1fa6672eed06b60440dd93116c0df0
Enderson Rufino Amorim <endersonrufino@gmail.com>
2021-07-28 22:11:45 -0300

    adicionado função que calcula os valores de deposito, saida e total

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,14 +9,35 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  return(
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  });
+
+  return (
     <Container>
       <div>
         <header>
           <p>Entradas</p>
           <img src={incomeIMG} alt="Entradas" />
         </header>
-        <strong>R$1000.00</strong>
+        <strong>{
+          new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -24,7 +45,13 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeIMG} alt="Saídas" />
         </header>
-        <strong>-R$500.00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -32,7 +59,11 @@ export function Summary() {
           <p>Total</p>
           <img src={totalIMG} alt="Total" />
         </header>
-        <strong>R$500.00</strong>
+        <strong>{
+          new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}</strong>
       </div>
     </Container>
   )
